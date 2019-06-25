948be1758bfc21f92fbdbbcec0b9f7d58dd5fa66
wangcheng <wangcheng@cechealth.cn>
2019-06-26 00:11:02 +0800

    添加批量查询image大小接口

--- api/Apis.py
+++ api/Apis.py
@@ -1,13 +1,15 @@
 # coding=utf-8
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import CephRBD
 import Model
+import json
 
 app = Flask(__name__)
 
 
 # 获取image的使用大小
-# request param:
+# request param
+# path param:
 # poolName                          string
 # imageName                         string
 #
@@ -24,11 +26,23 @@ def getImageUsageSizeByImageName(poolName, imageName):
                            CephRBD.getUsedSize(poolName, imageName))
     return Model.buildSuccessResponse("查询成功", data.__dict__)
 
-
-@app.route('/api/v1/ceph/rbd/', methods=['GET'])
-def getImageUsageSizeListByImageNameList():
-
-    pass
+# 批量获取image使用率
+# request param
+# path param:
+# poolName                          string
+# request body:
+# ["nameA", "nameB", "nameC"]
+@app.route('/api/v1/ceph/rbd/<poolName>/usages', methods=['GET'])
+def getImageUsageSizeListByImageNameList(poolName):
+    requestBody = request.get_data()
+    names = json.loads(requestBody)
+    imageInfoList = list()
+    for i in names:
+        print i
+        data = Model.ImageInfo(poolName, i, 10,
+                               CephRBD.getUsedSize(poolName, i))
+        imageInfoList.append(data.__dict__)
+    return Model.buildSuccessResponse("OK", imageInfoList)
 
 
 if __name__ == '__main__':
