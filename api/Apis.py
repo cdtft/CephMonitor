842edec84af03e7506b34c46a28e2f3726c41341
wangcheng <453451180@qq.com>
2019-06-25 10:39:56 +0800

    :art: init flask

---
+++ api/Apis.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+from flask import Flask, jsonify
+from ceph import CephRBD
+from model import Model
+
+app = Flask(__name__)
+
+
+# 获取image的使用大小
+# request param:
+# poolName                          string
+# imageName                         string
+#
+# response json data:
+# {
+#     "poolName": "k8s",            string
+#     "imageName": "45.45",         string
+#     "size": "10GB",               string
+#     "usage": "326MB"              string
+# }
+@app.route('/api/v1/ceph/rbd/<poolName>/<imageName>/usage', methods=['GET'])
+def getImageUsageSizeByImageName(poolName, imageName):
+    data = Model.ImageInfo(poolName, imageName, 10, CephRBD.getUsedSize(poolName, imageName))
+    # return Model.buildSuccessResponse("查询成功", data)
+    return jsonify(data.__dict__)
+
+
+
+
+@app.route('/api/v1/ceph/rbd/', )
+def getImageUsageSizeListByImageNameList():
+
+    pass
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(
+        host='0.0.0.0',
+        port='10086'
+    )
